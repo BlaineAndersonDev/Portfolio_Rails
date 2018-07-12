da111cbf52b111714cb886b4c46e2853b0cee09a
Blaine Anderson <BlaineAndersonDev@gmail.com>
2018-07-11 23:41:21 -0700

    Finalized Portfolio. Minimal extra touches before production

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home' => 'portfolio#home'
   get 'projects' => 'portfolio#projects'
   get 'skills' => 'portfolio#skills'
+  get 'about' => 'portfolio#about'
   get 'close' => 'portfolio#close'
   root 'portfolio#home'
 
