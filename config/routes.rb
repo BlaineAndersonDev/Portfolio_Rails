5335f81471df74db427771ed93179eda2f794bf5
Blaine Anderson <BlaineAndersonDev@gmail.com>
2018-07-11 18:04:31 -0700

    Added navigation for all windows. Added close.js.erb to allow for universal AJAX wrapper closes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home' => 'portfolio#home'
   get 'projects' => 'portfolio#projects'
   get 'skills' => 'portfolio#skills'
+  get 'close' => 'portfolio#close'
   root 'portfolio#home'
 
 end
